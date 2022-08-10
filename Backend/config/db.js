7cdb35586dadd0ecab3e528c8feb94c69b9bc0ec
Shir Segev <93188009+shirs12@users.noreply.github.com>
2022-08-10 18:24:16 +0300

    updated configurations

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -5,7 +5,7 @@ const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   database: process.env.DB_NAME,
-  password: "",
+  password: process.env.DB_PASSWORD,
 });
 
 module.exports = pool.promise();
