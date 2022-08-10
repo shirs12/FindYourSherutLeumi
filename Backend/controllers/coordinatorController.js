9f541f8a9dce5ada675ee62dd047c7a67654d9ab
Shir Segev <93188009+shirs12@users.noreply.github.com>
2022-08-10 11:14:53 +0300

    added coordinator  GET POST PUT methods

--- Backend/controllers/coordinatorController.js
+++ Backend/controllers/coordinatorController.js
@@ -10,3 +10,45 @@ exports.getAllCoordinators = async (req, res, next) => {
       next(err);
     }
   };
+
+// gets specific coordinator from db
+exports.getCoordinatorById = async (req, res, next) => {
+  const id = req.params.id;
+  try {
+    const [coordinator, _] = await pool.execute("CALL get_coordinator_by_id(?)", [id]);
+    res.status(200).json({ coordinator });
+  } catch (err) {
+    console.log(err);
+    next(err);
+  }
+};
+
+// POST - post new coordinator to the db
+exports.createNewCoordinator = async (req, res, next) => {
+  const { first_name,last_name,phone_number,email,coordinator_password,organization_name } = req.body;
+  try {
+    const coordinator = await pool.execute("CALL add_new_coordinator(?,?,?,?,?,?)",
+          [first_name,last_name,phone_number,email,coordinator_password,organization_name]);
+    res.status(201).json({ coordinator });
+  } catch (err) {
+    console.log(err);
+    next(err);
+  }
+};
+
+// PUT - put new update to a coordinator from the db
+exports.updateCoordinatorById = async (req, res, next) => {
+  const id = req.params.id;
+  const { first_name,last_name,phone_number,email,coordinator_password,organization_name } = req.body;
+    try {
+      const coordinator = await pool.execute("CALL update_coordinator(?,?,?,?,?,?,?)",
+          [id,first_name,last_name,phone_number,email,coordinator_password,organization_name]);
+      res.status(201).json({ coordinator });
+} catch (err) {
+  console.log(err);
+  next(err);
+}
+};
+
+
+
--- Backend/routes/coordinatorRoute.js
+++ Backend/routes/coordinatorRoute.js
@@ -4,6 +4,13 @@ const router = express.Router();
 
 // GET 
 router.route("/").get(coordinatorController.getAllCoordinators);
+router.route("/:id")
+      .get(coordinatorController.getCoordinatorById) // GET by id
+      .put(coordinatorController.updateCoordinatorById) // PUT by id
+
+
+// POST
+router.post("/add", coordinatorController.createNewCoordinator);
 
 module.exports = router;
 
