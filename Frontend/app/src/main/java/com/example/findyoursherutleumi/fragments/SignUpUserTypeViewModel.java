811ad5dad55733ec5668f4eff1c0bc3ad27165ac
Shir Segev <93188009+shirs12@users.noreply.github.com>
2022-08-24 16:55:00 +0300

    added 'SignUpFragment' and updated layout

---
+++ Frontend/app/src/main/java/com/example/findyoursherutleumi/fragments/SignUpUserTypeViewModel.java
@@ -0,0 +1,7 @@
+package com.example.findyoursherutleumi.fragments;
+
+import androidx.lifecycle.ViewModel;
+
+public class SignUpUserTypeViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
