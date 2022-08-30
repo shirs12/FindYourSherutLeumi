e222c23d9f6ba102624df8a299f2e9680e2c5bf5
Shir Segev <93188009+shirs12@users.noreply.github.com>
2022-08-30 22:30:52 +0300

    added 'SpinnerAdapter' file

---
+++ Frontend/app/src/main/java/com/example/findyoursherutleumi/adapters/SpinnerAdapter.java
@@ -0,0 +1,10 @@
+package com.example.findyoursherutleumi.adapters;
+
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import com.example.findyoursherutleumi.R;
+
+public class SpinnerAdapter {
+    
+}
