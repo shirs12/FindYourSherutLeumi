9144cf60c9834cbdb0d1b873bee14cf282df3049
Shir Segev <93188009+shirs12@users.noreply.github.com>
2022-09-08 22:51:29 +0300

    updated login and added 'homepage' fragment

--- Frontend/app/src/main/java/com/example/findyoursherutleumi/database/APIInterface.java
+++ Frontend/app/src/main/java/com/example/findyoursherutleumi/database/APIInterface.java
@@ -1,11 +1,15 @@
 package com.example.findyoursherutleumi.database;
 
 import com.example.findyoursherutleumi.models.Applicant;
+import com.example.findyoursherutleumi.models.User;
 
 import java.util.List;
 
 import retrofit2.Call;
+import retrofit2.http.Body;
 import retrofit2.http.GET;
+import retrofit2.http.Headers;
+import retrofit2.http.POST;
 import retrofit2.http.Path;
 
 public interface APIInterface {
@@ -15,7 +19,9 @@ public interface APIInterface {
     @GET("applicants/{id}")
     Call<Applicant> getApplicantById(@Path("id") int id);
 
-//    @POST("/signin")
+    @Headers("Content-Type: application/json")
+    @POST("login")
+    Call<User> authenticateUser(@Body String body);
 
 
 
--- Frontend/app/src/main/java/com/example/findyoursherutleumi/fragments/LoginFragment.java
+++ Frontend/app/src/main/java/com/example/findyoursherutleumi/fragments/LoginFragment.java
@@ -1,7 +1,6 @@
 package com.example.findyoursherutleumi.fragments;
 
 import androidx.fragment.app.FragmentTransaction;
-import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
 
@@ -13,13 +12,30 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.findyoursherutleumi.R;
+import com.example.findyoursherutleumi.database.APIClient;
+import com.example.findyoursherutleumi.database.APIInterface;
+import com.example.findyoursherutleumi.models.User;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class LoginFragment extends Fragment {
 
+    Button signInBtn;
+    EditText inputEmail;
+    EditText inputPassword;
     TextView signUpClickable;
+
+    APIInterface apiInterface;
 
     public static LoginFragment newInstance() {
         return new LoginFragment();
@@ -29,7 +45,53 @@ public class LoginFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_login, container, false);
+        apiInterface = APIClient.getInstance().create(APIInterface.class);
+
+        inputEmail = view.findViewById(R.id.email_address_input);
+        inputPassword = view.findViewById(R.id.password_input);
+
+        signInBtn = view.findViewById(R.id.sign_in_btn);
         signUpClickable = view.findViewById(R.id.sign_up_clickable);
+
+        signInBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (view.getId() == R.id.sign_in_btn){
+                    JSONObject paramObject = new JSONObject();
+                    try {
+                        paramObject.put("email", inputEmail.toString());
+                        paramObject.put("password", inputPassword.toString());
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                    Call<User> call = apiInterface.authenticateUser(paramObject.toString());
+                    call.enqueue(new Callback<User>() {
+                        @Override
+                        public void onResponse(Call<User> call, Response<User> response) {
+                            if(!response.isSuccessful()) {
+                                System.out.println("1");
+                                System.out.println(response.body());
+                                System.out.println(paramObject);
+                                Toast.makeText(getContext(), "Connection Failed. try again later...", Toast.LENGTH_SHORT).show();
+                            }else {
+                                Fragment newFragment = new HomePageFragment();
+                                FragmentTransaction transaction = getFragmentManager().beginTransaction();
+                                transaction.replace(R.id.fragmentContainerView, newFragment);
+                                transaction.addToBackStack(null);
+                                transaction.commit();
+                            }
+                        }
+
+                        @Override
+                        public void onFailure(Call<User> call, Throwable t) {
+                            System.out.println("2");
+                            Toast.makeText(getContext(), "Connection Failed. try again later...", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                }
+            }
+        });
+
         signUpClickable.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
