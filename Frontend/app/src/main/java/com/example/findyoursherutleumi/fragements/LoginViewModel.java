0f52b1394edc4b89856a68ca8bff993f77a89e6e
Shir Segev <93188009+shirs12@users.noreply.github.com>
2022-09-21 13:22:20 +0300

    unnecessary

--- Frontend/app/src/main/java/com/example/findyoursherutleumi/fragements/LoginViewModel.java
+++
@@ -1,7 +0,0 @@
-package com.example.findyoursherutleumi.fragements;
-
-import androidx.lifecycle.ViewModel;
-
-public class LoginViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
-}
