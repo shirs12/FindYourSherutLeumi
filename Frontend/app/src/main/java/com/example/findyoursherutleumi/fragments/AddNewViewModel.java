8b9e7b390c1e908c3aa14147386e613a68269f76
Shir Segev <93188009+shirs12@users.noreply.github.com>
2022-09-21 15:11:41 +0300

    Delete AddNewViewModel.java

--- Frontend/app/src/main/java/com/example/findyoursherutleumi/fragments/AddNewViewModel.java
+++
@@ -1,7 +0,0 @@
-package com.example.findyoursherutleumi.fragments;
-
-import androidx.lifecycle.ViewModel;
-
-public class AddNewViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
-}
