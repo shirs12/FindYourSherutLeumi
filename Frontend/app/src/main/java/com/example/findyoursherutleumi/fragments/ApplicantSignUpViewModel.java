e6ba172da0f1599b931a2e42c11cc33a1cb997cd
Shir Segev <93188009+shirs12@users.noreply.github.com>
2022-09-21 13:24:46 +0300

    Delete ApplicantSignUpViewModel.java

--- Frontend/app/src/main/java/com/example/findyoursherutleumi/fragments/ApplicantSignUpViewModel.java
+++
@@ -1,7 +0,0 @@
-package com.example.findyoursherutleumi.fragments;
-
-import androidx.lifecycle.ViewModel;
-
-public class ApplicantSignUpViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
-}
