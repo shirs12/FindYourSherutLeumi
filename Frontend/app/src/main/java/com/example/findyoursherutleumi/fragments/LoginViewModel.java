3549663b2774e818d1ad0c5908f69f32367fd0ea
Shir Segev <93188009+shirs12@users.noreply.github.com>
2022-09-21 13:24:32 +0300

    Delete LoginViewModel.java

--- Frontend/app/src/main/java/com/example/findyoursherutleumi/fragments/LoginViewModel.java
+++
@@ -1,7 +0,0 @@
-package com.example.findyoursherutleumi.fragments;
-
-import androidx.lifecycle.ViewModel;
-
-public class LoginViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
-}
