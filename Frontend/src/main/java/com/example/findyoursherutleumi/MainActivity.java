101c4411fab79551bf60bec93ac94f0f784d7075
Shir Segev <93188009+shirs12@users.noreply.github.com>
2022-09-22 00:04:04 +0300

    added 'SettingsFragment'

---
+++ Frontend/src/main/java/com/example/findyoursherutleumi/MainActivity.java
@@ -0,0 +1,178 @@
+package com.example.findyoursherutleumi;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.findyoursherutleumi.adapters.ServicesAdapter;
+import com.example.findyoursherutleumi.database.APIClient;
+import com.example.findyoursherutleumi.database.APIInterface;
+import com.example.findyoursherutleumi.fragments.AddNewServiceFragment;
+import com.example.findyoursherutleumi.fragments.LoginFragment;
+import com.example.findyoursherutleumi.fragments.SettingsFragment;
+import com.example.findyoursherutleumi.models.Applicant;
+
+import java.util.List;
+import java.util.Locale;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+//    APIInterface apiInterface;
+//    private Button button1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        loadLocale();
+        setContentView(R.layout.activity_main);
+
+        ActionBar actionBar = getSupportActionBar();
+        assert actionBar != null;
+        actionBar.setTitle(getResources().getString(R.string.app_name));
+
+
+
+//        button1 = findViewById(R.id.button1);
+//        apiInterface = APIClient.getInstance().create(APIInterface.class);
+//
+//        button1.setOnClickListener(view -> {
+//            Call<List<Applicant>> call = apiInterface.getAllApplicants();
+//            call.enqueue(new Callback<List<Applicant>>() {
+//                @Override
+//                public void onResponse(@NonNull Call<List<Applicant>> call, @NonNull Response<List<Applicant>> response) {
+//                    if(!response.isSuccessful()) Log.e(TAG, "onResponse: code : " + response.code());
+//                    if(response.isSuccessful()) {
+//                        Log.e(TAG, "onResponse: code : " + response.code());
+//                        assert response.body() != null;
+//                        Log.e(TAG, "onResponse: " + response.body().get(1));
+//                    }
+//                }
+//
+//                @Override
+//                public void onFailure(Call<List<Applicant>> call, Throwable t) {
+//                    Log.e(TAG, "onFailure: " + t.getMessage());
+//                }
+//            });
+//        });
+
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.nav_menu, menu);
+        MenuItem languagesItem = menu.findItem(R.id.languages_btn);
+        MenuItem logoutItem = menu.findItem(R.id.logout_item);
+        MenuItem settingsItem = menu.findItem(R.id.settings_item);
+
+        languagesItem.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem menuItem) {
+                showChangeLanguageDialog();
+                return true;
+            }
+        });
+
+        logoutItem.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem menuItem) {
+                Fragment newFragment = new LoginFragment();
+                assert getFragmentManager() != null;
+                FragmentManager fragmentManager = getSupportFragmentManager();
+                FragmentTransaction transaction = fragmentManager.beginTransaction();
+                transaction.replace(R.id.fragmentContainerView, newFragment).disallowAddToBackStack();
+                transaction.commit();
+
+                // clears fragment's back stack to prevent user from going back after logging out.
+                int count = fragmentManager.getBackStackEntryCount();
+                for(int i = 0; i < count; ++i) {
+                    fragmentManager.popBackStackImmediate();
+                }
+                return true;
+            }
+        });
+
+        settingsItem.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem menuItem) {
+                Fragment newFragment = new SettingsFragment();
+                FragmentManager fragmentManager = getSupportFragmentManager();
+//                Bundle bundle = new Bundle();
+//                bundle.putInt("id", id);
+//                newFragment.setArguments(bundle);
+
+                // TODO: send user id to setting fragment to show user's details
+                assert getFragmentManager() != null;
+
+                FragmentTransaction transaction = fragmentManager.beginTransaction();
+                transaction.replace(R.id.fragmentContainerView, newFragment);
+                transaction.addToBackStack(null);
+                transaction.commit();
+
+                return true;
+            }
+        });
+
+        return true;
+    }
+    private void showChangeLanguageDialog() {
+        final String [] languagesLst = {"עברית", "English"};
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setTitle("Choose Language...");
+        builder.setSingleChoiceItems(languagesLst, -1, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                if (i == 0) {
+                    setLocale("iw");
+                    recreate();
+                }
+                else if (i == 1) {
+                    setLocale("en");
+                    recreate();
+                }
+                dialogInterface.dismiss();
+            }
+        });
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+    }
+
+    private void setLocale(String lang) {
+        Locale locale = new Locale(lang);
+        Locale.setDefault(locale);
+        Configuration config = new Configuration();
+        config.setLocale(locale);
+        getBaseContext().getResources().updateConfiguration(config, getBaseContext().getResources().getDisplayMetrics());
+        SharedPreferences.Editor editor = getSharedPreferences("Settings", MODE_PRIVATE).edit();
+        editor.putString("My_Lang",lang);
+        editor.apply();
+    }
+
+    public void loadLocale(){
+        SharedPreferences preferences = getSharedPreferences("Settings", MODE_PRIVATE);
+        String language = preferences.getString("My_Lang","");
+        setLocale(language);
+    }
+
+
+}
---
+++ Frontend/src/main/java/com/example/findyoursherutleumi/fragments/HomePageFragment.java
@@ -0,0 +1,188 @@
+package com.example.findyoursherutleumi.fragments;
+
+import androidx.fragment.app.FragmentTransaction;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+
+import android.annotation.SuppressLint;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.inputmethod.EditorInfo;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.SearchView;
+import android.widget.Spinner;
+
+import com.example.findyoursherutleumi.R;
+import com.example.findyoursherutleumi.adapters.ServicesAdapter;
+import com.example.findyoursherutleumi.models.ServicePartial;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.List;
+import java.util.Objects;
+
+public class HomePageFragment extends Fragment {
+
+    private int userTypeId;
+    private String userEmail;
+
+    private HomePageViewModel mViewModel;
+    RecyclerView recyclerView;
+    ServicesAdapter servicesAdapter;
+    FloatingActionButton addServiceBtn;
+    private EditText searchBar;
+
+    public static HomePageFragment newInstance() {
+        return new HomePageFragment();
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(HomePageViewModel.class);
+        mViewModel.init();
+    }
+
+    @SuppressLint("NotifyDataSetChanged")
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_home_page, container, false);
+        setHasOptionsMenu(true);
+        addServiceBtn = view.findViewById(R.id.add_service_btn);
+
+        assert getArguments() != null;
+        userEmail = getArguments().getString("email");
+        userTypeId = getArguments().getInt("typeId");
+        System.out.println(userTypeId);
+        if (userTypeId == 1) {
+            addServiceBtn.setVisibility(View.GONE);
+        }
+
+        recyclerView = view.findViewById(R.id.services_lst);
+        mViewModel.getServices().observe(getViewLifecycleOwner(), new Observer<List<ServicePartial>>() {
+            @SuppressLint("NotifyDataSetChanged")
+            @Override
+            public void onChanged(List<ServicePartial> servicePartials) {
+                servicesAdapter.updateServicesList(servicePartials);
+                servicesAdapter.notifyDataSetChanged();
+            }
+        });
+
+        initRecyclerView();
+
+        addServiceBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Fragment newFragment = new AddNewServiceFragment();
+                Bundle bundle = new Bundle();
+                bundle.putString("email", userEmail);
+                newFragment.setArguments(bundle);
+
+                assert getFragmentManager() != null;
+                FragmentTransaction transaction = getFragmentManager().beginTransaction();
+                transaction.replace(R.id.fragmentContainerView, newFragment);
+                transaction.addToBackStack(null);
+                transaction.commit();
+            }
+        });
+
+        searchBar = view.findViewById(R.id.search_bar);
+        searchBar.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                servicesAdapter.notifyDataSetChanged();
+            }
+
+            @SuppressLint("NotifyDataSetChanged")
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                servicesAdapter.filter(charSequence);
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
+        servicesAdapter.notifyDataSetChanged();
+        return view;
+    }
+
+    private void initRecyclerView() {
+        servicesAdapter = new ServicesAdapter(getLayoutInflater(), mViewModel.getServices().getValue());
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this.getContext());
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setAdapter(servicesAdapter);
+    }
+
+    @SuppressLint("NotifyDataSetChanged")
+    public void updateAdapter() {
+        servicesAdapter.notifyDataSetChanged();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        updateAdapter();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        updateAdapter();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        updateAdapter();
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        updateAdapter();
+    }
+
+    //    @Override
+//    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+//        super.onCreateOptionsMenu(menu,inflater);
+//        inflater.inflate(R.menu.nav_menu, menu);
+//        MenuItem searchItem = menu.findItem(R.id.action_search);
+//        SearchView searchView = (SearchView) searchItem.getActionView();
+//
+//        searchView.setImeOptions(EditorInfo.IME_ACTION_DONE);
+//
+//        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+//            @Override
+//            public boolean onQueryTextSubmit(String s) {
+//                return false;
+//            }
+//
+//            @Override
+//            public boolean onQueryTextChange(String s) {
+//                servicesAdapter.getFilter().filter(s);
+//                return false;
+//            }
+//        });
+//
+//    }
+}
