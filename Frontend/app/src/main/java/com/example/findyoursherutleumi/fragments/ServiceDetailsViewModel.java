bcc7b92494441925443308c37f137cdd1eed38dd
Shir Segev <93188009+shirs12@users.noreply.github.com>
2022-09-21 13:24:01 +0300

    unnecessary

--- Frontend/app/src/main/java/com/example/findyoursherutleumi/fragments/ServiceDetailsViewModel.java
+++
@@ -1,7 +0,0 @@
-package com.example.findyoursherutleumi.fragments;
-
-import androidx.lifecycle.ViewModel;
-
-public class ServiceDetailsViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
-}
