cf77b567d2ebdac751370dad3a68cf3831d90128
Shir Segev <93188009+shirs12@users.noreply.github.com>
2022-10-06 14:23:34 +0300

    Delete Frontend/src directory

--- Frontend/src/main/java/com/example/findyoursherutleumi/FragmentToActivity.java
+++
@@ -1,9 +0,0 @@
-package com.example.findyoursherutleumi;
-
-import com.example.findyoursherutleumi.models.User;
-
-// send data from fragment to activity
-public interface FragmentToActivity {
-    void communicate(User user);
-
-}
