096a9fc012cab3d3150808d6953055fec23ad355
Shir Segev <93188009+shirs12@users.noreply.github.com>
2023-01-07 20:26:26 +0200

    added some comments

--- Backend/config/nodemailer.js
+++ Backend/config/nodemailer.js
@@ -1,5 +1,6 @@
 const nodemailer = require('nodemailer');
 
+// creates a transporter to allow easy email sendings
 const transporter = nodemailer.createTransport({
     service: 'zoho',
     auth: {
--- Backend/routes/generalRoute.js
+++ Backend/routes/generalRoute.js
@@ -2,9 +2,9 @@ const express = require("express");
 const generalController = require("../controllers/generalController");
 const router = express.Router();
 
-router.route("/:email").get(generalController.getUserByEmail);
-router.route("/").post(generalController.authenticateUser);
-router.route("/forgotpassword/:email").put(generalController.updateUserPassword);
+router.route("/:email").get(generalController.getUserByEmail); // GET
+router.route("/").post(generalController.authenticateUser); // POST
+router.route("/forgotpassword/:email").put(generalController.updateUserPassword); // PUT
 
 
 module.exports = router;
